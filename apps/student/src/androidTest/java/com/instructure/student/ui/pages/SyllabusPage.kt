4b516b4312ccc473fd8e96b184c2a26d579c2780
Joe Hoag <jhoag@instructure.com>
2020-07-06 12:40:29 -0600

    MBL-14506: Initial fix
    Waiting for PTR functionality to be restored to syllabus page.

--- apps/student/src/androidTest/java/com/instructure/student/ui/pages/SyllabusPage.kt
+++ apps/student/src/androidTest/java/com/instructure/student/ui/pages/SyllabusPage.kt
@@ -17,6 +17,7 @@
 package com.instructure.student.ui.pages
 
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import com.instructure.canvas.espresso.scrollRecyclerView
 import com.instructure.espresso.assertDisplayed
@@ -33,7 +34,7 @@ open class SyllabusPage : BasePage(R.id.syllabusPage) {
     }
 
     fun assertEmptyView() {
-        onView(withId(R.id.syllabusEmptyView)).assertDisplayed()
+        onView(allOf(withId(R.id.emptyView), isDisplayed())) .assertDisplayed()
     }
 
     fun refresh() {
